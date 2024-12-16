66b0e9eafec29e362166d1c0486f318aff4c5e2f
Renato Zukeran <renato.zukeran@servexternos.santander.pt>
2024-12-16 18:08:57 +0000

    improve code

--- src/main/java/com/tiojiro/Main.java
+++ src/main/java/com/tiojiro/Main.java
@@ -1,6 +1,7 @@
 package com.tiojiro;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,23 +10,22 @@ import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 import static com.tiojiro.Constants.*;
 
 public class Main {
     public static void main(String[] args) {
-        while (true) {
-            printConsoleMsg(BEGIN);
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("headless");
+        StringBuilder checkDivErrorText = new StringBuilder();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        do {
             ChromeDriver driver = new ChromeDriver(options);
-
             try {
+                printConsoleMsg(BEGIN);
                 driver.get("https://www.sef.pt/pt/Pages/Homepage.aspx");
-                String actualTitle = driver.getTitle();
-                printConsoleMsg(LOADED_PAGE + actualTitle);
+                String currentTitle = driver.getTitle();
+                printConsoleMsg(LOADED_PAGE + currentTitle);
 
                 WebElement loginLauncher = driver.findElement(new By.ByClassName("login-launcher"));
                 loginLauncher.click();
@@ -38,31 +38,23 @@ public class Main {
                 WebElement btnLogin = driver.findElement(new By.ById("btnLogin"));
                 btnLogin.click();
 
-                actualTitle = driver.getTitle();
+                currentTitle = driver.getTitle();
 
-                if (HOME.equals(actualTitle.trim())) {
+                if (HOME.equals(currentTitle.trim())) {
                     printConsoleMsg(LOGIN_OK);
 
-                    WebElement renovacaoAutomatica = driver.findElement(new By.ById("renovacaoAutomaticaLink"));
-                    renovacaoAutomatica.click();
+                    WebElement optionAutoRenew = driver.findElement(new By.ById("renovacaoAutomaticaLink"));
+                    optionAutoRenew.click();
 
                     WebElement txtAuthPanelPassword = driver.findElement(new By.ById("txtAuthPanelPassword"));
                     WebElement txtAuthPanelDocument = driver.findElement(new By.ById("txtAuthPanelDocument"));
                     txtAuthPanelPassword.sendKeys(PASS);
                     txtAuthPanelDocument.sendKeys(DOCUMENT);
 
-                    WebElement btnAutenticaUtilizador = driver.findElement(new By.ById("btnAutenticaUtilizador"));
-                    btnAutenticaUtilizador.click();
+                    WebElement btnLoginSubmit = driver.findElement(new By.ById("btnAutenticaUtilizador"));
+                    btnLoginSubmit.click();
 
-                    WebElement divError = driver.findElement(new By.ById("ctl00_ctl53_g_49abea8d_9129_4f50_bf46_33662dfac0a6_ctl00_lblAuthError"));
-                    if (Objects.nonNull(divError)) {
-                        printConsoleMsg(divError.getText());
-                        if (!divError.getText().trim().startsWith(CHECK_STRING_ERROR)) {
-                            checkSefWebsiteAlert();
-                        }
-                    } else {
-                        checkSefWebsiteAlert();
-                    }
+                    checkErrorText(driver, checkDivErrorText);
                 } else {
                     printConsoleMsg(LOGIN_ERROR);
                 }
@@ -73,24 +65,41 @@ public class Main {
             } finally {
                 driver.quit();
             }
+        } while (checkDivErrorText.toString().startsWith(CHECK_STRING_ERROR));
+    }
+
+    private static void checkErrorText(ChromeDriver driver, StringBuilder checkDivErrorText) {
+        try {
+            WebElement divError = driver.findElement(new By.ById("ctl00_ctl53_g_49abea8d_9129_4f50_bf46_33662dfac0a6_ctl00_lblAuthError"));
+            checkDivErrorText.append(divError.getText().trim());
+            printConsoleMsg(checkDivErrorText.toString());
+            if (!checkDivErrorText.toString().startsWith(CHECK_STRING_ERROR)) {
+                checkSefWebsiteAlert();
+            }
+        } catch (NoSuchElementException e) {
+            checkSefWebsiteAlert();
         }
     }
-    private static void checkSefWebsiteAlert () {
+
+    private static void checkSefWebsiteAlert() {
         printConsoleMsg(OPEN_COLOR_RED + CHECK_SEF_WEBSITE + CLOSE_COLOR_RED);
         playWindowsAlertSong();
         openPopUpAlertMsg();
     }
-    private static void printConsoleMsg (String msg) {
+
+    private static void printConsoleMsg(String msg) {
         DateTimeFormatter sdf = DateTimeFormatter.ofPattern(DATE_PATTERN);
         String log = OPEN_BRACKET + LocalDateTime.now().format(sdf) + CLOSE_BRACKET;
         System.out.println(log + msg);
     }
-    private static void openPopUpAlertMsg () {
-        JFrame j=new JFrame();
+
+    private static void openPopUpAlertMsg() {
+        JFrame j = new JFrame();
         j.setAlwaysOnTop(true);
         JOptionPane.showMessageDialog(j, CHECK_SEF_WEBSITE);
     }
-    private static void playWindowsAlertSong () {
+
+    private static void playWindowsAlertSong() {
         final Runnable runnable = (Runnable) Toolkit.getDefaultToolkit().getDesktopProperty("win.sound.exclamation");
         if (runnable != null)
             runnable.run();
